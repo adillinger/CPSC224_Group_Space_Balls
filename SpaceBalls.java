b7248f5814cc49f05a5aa5afc1f7cd59e9fe82a1
adillinger <38118463+adillinger@users.noreply.github.com>
2018-04-19 21:43:53 -0700

    Create SpaceBalls.java
    It isn't finished, but this is what I've started for the main screen.

---
+++ SpaceBalls.java
@@ -0,0 +1,25 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class Game{
+  public Game(JPanel welcomePanel){	  
+	JLabel players = new JLabel("How many players are there? ");  
+	players.setBounds(50, 100, 200, 30);
+    JTextField gameSetup = new JTextField(2);
+    gameSetup.setBounds(250,100,100,30);
+    gameSetup.setEditable(true);
+    welcomePanel.add(gameSetup);
+    welcomePanel.add(players);
+    int numberOfPlayers = Integer.parseInt(gameSetup.getText());
+    players.setVisible(false);
+    gameSetup.setVisible(false);
+  }
+
+  public void playGame(){
+
+  }
+
+  /* private scoreGame(){
+    ScoreCard scoreCard = new ScoreCard();
+  } */
+}
